5b151ed127e9f26cb7fbd5a16ed539b4bbc722ba
dianaaadumitru <dianad2601@gmail.com>
2023-04-07 15:15:15 +0300

    todo

---
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.backend.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+}

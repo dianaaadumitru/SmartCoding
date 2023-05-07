d507217be65de769cb2b6bfe1ced728e99c165cf
dianaadumitru <100534964+dianaaadumitru@users.noreply.github.com>
2023-05-07 20:12:16 +0300

    frontend app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './login/login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>main page</h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;

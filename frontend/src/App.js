7463befc529e285849357bd6bd93050a6efeb6c6
dianaadumitru <100534964+dianaaadumitru@users.noreply.github.com>
2023-05-11 13:00:02 +0300

    hello world react project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,93 +1,23 @@
-import AddItem from './AddItem';
+import logo from './logo.svg';
 import './App.css';
-import Content from './Content';
-import Footer from './Footer';
-import Header from './Header';
-import SearchItem from './SearchItem';
-
-import { useEffect, useState } from "react";
 
 function App() {
-  const API_URL = "http://localhost:3500"
-
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
-  const [search, setSearch] = useState("");
-  const [fetcherror, setFetchError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const response = await fetch(API_URL + "/items");
-        if (!response.ok) throw Error("did not received expected data");
-        const listItems = await response.json();
-        setItems(listItems);
-        setFetchError(null);
-      } catch (err) {
-        setFetchError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    setTimeout(() => {
-      (async () => await fetchItems())();
-    }, 2000)
-  }, [])
-
-  const setANdsSaveItems = (listItems) => {
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-
-  const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id, checked: false, item };
-    const listItems = [...items, myNewItem];
-    setANdsSaveItems(listItems);
-  }
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
-    setANdsSaveItems(listItems);
-  }
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id != id);
-    setANdsSaveItems(listItems);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!newItem) return;
-    addItem(newItem);
-    setNewItem('');
-  }
-
-
   return (
     <div className="App">
-      <Header title="groceries list" />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      />
-      <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        handleSubmit={handleSubmit}
-      />
-      <main>
-        {isLoading && <p>Loading items...</p>}
-        {fetcherror && <p>{'Error: ' + fetcherror}</p>}
-        {!fetcherror && !isLoading && <Content
-          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />}
-      </main>
-      <Footer length={items.length} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

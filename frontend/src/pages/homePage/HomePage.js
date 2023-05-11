067bee9dbe6b9d19ffcbc07bbfb8287fc06bca07
dianaadumitru <100534964+dianaaadumitru@users.noreply.github.com>
2023-05-11 18:44:53 +0300

    started main page

---
+++ frontend/src/pages/homePage/HomePage.js
@@ -0,0 +1,21 @@
+import './HomePage.css'
+// import backgroundImage from 'src/resources/background.jpg'
+import backgroundImage from '../../resources/background2.jpg'
+import { Link } from 'react-router-dom';
+import HomePageNavBar from './homePageNavBar/HomePageNavBar';
+
+
+function HomePage() {
+    return (
+        <body>
+            <HomePageNavBar />
+            <div className='content-block'>
+                <h1 className='page-heading page-heading--small'>Smart coding</h1>
+                <p className='page-content page-content--small'>Solve problems, sharpen skills, succeed in <br />programming.</p>
+                <button class="create-account-btn">Create Account</button>
+            </div>
+        </body>
+    )
+}
+
+export default HomePage;
---
+++ frontend/src/pages/homePage/homePageNavBar/HomePageNavBar.js
@@ -0,0 +1,17 @@
+import './HomePageNavBar.css'
+
+function HomePageNavBar() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#">Explore</a></li>
+                    <li><a href="#">Developer</a></li>
+                    <li><a href="#">Sign in</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default HomePageNavBar;

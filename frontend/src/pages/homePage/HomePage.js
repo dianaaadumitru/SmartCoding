2deceb04ca40f36370424a3c4155cc567030c2e2
dianaadumitru <100534964+dianaaadumitru@users.noreply.github.com>
2023-05-27 16:54:42 +0300

    front: home navbar

--- frontend/src/pages/homePage/HomePage.js
+++ frontend/src/pages/homePage/HomePage.js
@@ -7,48 +7,64 @@ import TopCourses from 'components/Course/TopCourses/TopCourses';
 import DeveloperComponent from 'components/Developer/DeveloperComponent';
 
 function HomePage() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const targetRef = useRef(null);
+  const exploreRef = useRef(null);
+  const developerRef = useRef(null);
 
   useEffect(() => {
-    if (window.location.hash === '#target') {
-      targetRef.current.scrollIntoView({ behavior: 'smooth' });
+    if (window.location.hash === '#explore') {
+      exploreRef.current.scrollIntoView({ behavior: 'smooth' });
+    } else if (window.location.hash === '#developer') {
+      developerRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   }, []);
 
-  const handleClick = async () => navigate('/createAccount')
-  const handleClickExploreCatalog = async () => navigate('/signin')
+  const handleExploreClick = () => {
+    window.location.hash = 'explore';
+    exploreRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handleDeveloperClick = () => {
+    window.location.hash = 'developer';
+    developerRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handleClick = () => navigate('/createAccount');
+  const handleClickExploreCatalog = () => navigate('/signin');
 
   return (
     <>
       <section className='homepage-section'>
-        <HomePageNavBar targetRef={targetRef} />
+        <HomePageNavBar
+          onExploreClick={handleExploreClick}
+          onDeveloperClick={handleDeveloperClick}
+        />
         <div className='homepage-content-wrapper'>
           <div className='content-block'>
             <div className='center'>
               <h1 className='page-heading'>Smart coding</h1>
               <p className='page-content'>Solve problems, sharpen skills, succeed in programming.</p>
-              <button className="create-account-btn" onClick={() => handleClick()}>Create Account</button>
+              <button className="create-account-btn" onClick={handleClick}>Create Account</button>
             </div>
           </div>
         </div>
       </section>
       <section className='explore-section'>
-        <h2 ref={targetRef} className='start-learning'>Start learning</h2>
-        <p className='top-courses'>Top courses: </p>
+        <h2 ref={exploreRef} className='start-learning'>Start learning</h2>
+        <p className='top-courses'>Top courses:</p>
         <TopCourses />
-        <button className="create-account-btn" onClick={() => handleClickExploreCatalog()}>Explore full catalog</button>
-        <p className='top-courses'>Top problems: </p>
+        <button className="create-account-btn" onClick={handleClickExploreCatalog}>Explore full catalog</button>
+        <p className='top-courses'>Top problems:</p>
         <TopProblems />
-        <button className="create-account-btn" onClick={() => handleClickExploreCatalog()}>Explore full catalog</button>
+        <button className="create-account-btn" onClick={handleClickExploreCatalog}>Explore full catalog</button>
       </section>
       <section className='developer-section'>
-        <h2 className='start-learning'>Developer</h2>
+        <h2 ref={developerRef} className='start-learning'>Developer</h2>
         <DeveloperComponent />
       </section>
     </>
-  )
+  );
 }
 
 export default HomePage;
--- frontend/src/pages/homePage/homePageNavBar/HomePageNavBar.js
+++ frontend/src/pages/homePage/homePageNavBar/HomePageNavBar.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import './HomePageNavBar.css';
 
-import './HomePageNavBar.css'
-
-function HomePageNavBar(props) {
-    function handleListItemClick() {
-        props.targetRef.current.scrollIntoView({ behavior: 'smooth' });
-      }
-
-
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li onClick={handleListItemClick}>Explore</li>
-                    <li><a href="#">Developer</a></li>
-                    <li><a href="/signin">Sign in</a></li>
-                </ul>
-            </nav>
-        </div>
-    )
+function HomePageNavBar({ onExploreClick, onDeveloperClick }) {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li onClick={onExploreClick}>Explore</li>
+          <li onClick={onDeveloperClick}>Developer</li>
+          <li><a href="/signin">Sign in</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default HomePageNavBar;

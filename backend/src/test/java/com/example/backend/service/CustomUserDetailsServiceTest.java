38b421b2091b7c575c09beae2ea5f34e27e834f5
dianaadumitru <100534964+dianaaadumitru@users.noreply.github.com>
2023-05-28 15:34:17 +0300

    customUserDetails service tests

--- backend/src/test/java/com/example/backend/service/CustomUserDetailsServiceTest.java
+++ backend/src/test/java/com/example/backend/service/CustomUserDetailsServiceTest.java
@@ -1,11 +1,82 @@
 package com.example.backend.service;
 
+import com.example.backend.entity.Role;
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomUserDetailsServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private CustomUserDetailsService userDetailsService;
+
+    @Test
+    public void testLoadUserByUsername_UsernameFound() {
+        // given
+        String usernameOrEmail = "johndoe@example.com";
+        String password = "password";
+        String role = "ROLE_USER";
+
+        User user = User.builder()
+                .email("johndoe@example.com")
+                .password(password)
+                .build();
+
+        Role userRole = new Role();
+        userRole.setRole(role);
+        user.setRoles(Collections.singleton(userRole));
+
+        when(userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)).thenReturn(Optional.of(user));
+
+        // when
+        UserDetails userDetails = userDetailsService.loadUserByUsername(usernameOrEmail);
+
+        // then
+        assertEquals(usernameOrEmail, userDetails.getUsername());
+        assertEquals(password, userDetails.getPassword());
+
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+
+        Set<GrantedAuthority> authoritiesSet = authorities.stream()
+                .map(authority -> (GrantedAuthority) authority)
+                .collect(Collectors.toSet());
+        assertEquals(1, authorities.size());
+        GrantedAuthority authority = authorities.iterator().next();
+        assertEquals(role, authority.getAuthority());
+    }
+
+    @Test
+    public void testLoadUserByUsername_UsernameNotFound() {
+        // given
+        String usernameOrEmail = "nonexistentUser";
+
+        // when
+        when(userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(UsernameNotFoundException.class, () -> {
+            userDetailsService.loadUserByUsername(usernameOrEmail);
+        });
+    }
 
 }

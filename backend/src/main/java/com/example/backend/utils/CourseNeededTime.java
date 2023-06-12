8c62cae55bfc4383ce751e2144409579fe48acd7
dianaadumitru <100534964+dianaaadumitru@users.noreply.github.com>
2023-06-12 20:17:56 +0300

    fixes

--- backend/src/main/java/com/example/backend/utils/CourseNeededTime.java
+++ backend/src/main/java/com/example/backend/utils/CourseNeededTime.java
@@ -6,26 +6,34 @@ import java.util.regex.Pattern;
 public class CourseNeededTime {
     public static double extractValueFromString(String input) {
         System.out.println("extracting..." + input);
-        String pattern = "(\\d+\\.\\d+)";
+        String pattern = "(\\d{1,2})(?::(\\d{1,2}))? hours";
         Pattern regex = Pattern.compile(pattern);
 
         Matcher matcher = regex.matcher(input);
         if (matcher.find()) {
-            String match = matcher.group();
-            System.out.println("im here: " + match);
-            return Double.parseDouble(match);
+            String hours = matcher.group(1);
+            String minutes = matcher.group(2);
+
+            int totalMinutes = 0;
+            if (hours != null) {
+                totalMinutes += Integer.parseInt(hours) * 60;
+            }
+            if (minutes != null) {
+                totalMinutes += Integer.parseInt(minutes);
+            }
+
+            double totalTime = totalMinutes / 60.0;
+            System.out.println("im here: " + totalTime);
+            return totalTime;
         }
         return 0.0;
     }
 
     public static String convertDoubleIntoHoursAndMinutes(double givenTime) {
-        int wholeHours = (int) givenTime;
-        int minutes = (int)  Math.round(((givenTime - wholeHours) * 100));
+        int totalMinutes = (int) Math.round(givenTime * 60);
+        int wholeHours = totalMinutes / 60;
+        int minutes = totalMinutes % 60;
 
-        if (minutes >= 60) {
-            wholeHours += 1;
-            minutes -= 60;
-        }
         return String.format("%02d:%02d", wholeHours, minutes);
     }
 }
